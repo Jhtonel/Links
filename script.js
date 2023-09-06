943e8077cf292e547c254800be666ed922eb9b6a
jhtonel <jhtonel@gmail.com>
2023-09-06 17:35:32 -0300

    add new features

--- script.js
+++ script.js
@@ -12,3 +12,52 @@ function toggleMode() {
   */
 }
 
+const dynamicText = document.querySelector("#makedby span")
+const words = ["amor", "exelência"]
+
+let wordIndex = 0
+let charIndex = 0
+let isDeleting = false
+
+const typeEffect = () => {
+  const currentWord = words[wordIndex]
+  const currentChar = currentWord.substring(0, charIndex)
+  dynamicText.textContent = currentChar
+  dynamicText.classList.add("stop-blinking")
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    charIndex++
+    setTimeout(typeEffect, 150)
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--
+    setTimeout(typeEffect, 100)
+  } else {
+    isDeleting = !isDeleting
+    dynamicText.classList.remove("stop-blinking")
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex
+    setTimeout(typeEffect, 1600)
+  }
+  if (currentWord === "amor") {
+    if (charIndex === currentWord.length) {
+      setTimeout(function () {
+        dynamicText.innerHTML =
+          '<ion-icon class="ionicon" name="heart"></ion-icon>'
+      }, 350)
+      setTimeout(() => {
+        dynamicText.innerHTML = currentWord
+      }, 1700)
+    }
+  }
+  if (currentWord === "exelência") {
+    if (charIndex === currentWord.length) {
+      setTimeout(function () {
+        dynamicText.innerHTML =
+          '<ion-icon class="ionicon jose" name="glasses"></ion-icon>'
+      }, 350)
+      setTimeout(() => {
+        dynamicText.innerHTML = currentWord
+      }, 1700)
+    }
+  }
+}
+typeEffect()
